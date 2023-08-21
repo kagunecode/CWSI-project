f0ed2fc44771c14cde053a671d995489d7f627ec
kagunecode <cesar.acb98@gmail.com>
2023-08-21 16:09:34 -0500

    Added normal photo key. Home function is working. 0.5.1.
    The drone can now take normal photos as well, without plotting any
    temperature values and saving them on the same directory.
    
    The home function has been tested and it's working. Please, be careful
    when using this drone as it will go high (as high as it can) and will
    return home at max speed. It can be a bit scary, so please be aware
    of this function and use it only when needed.
    
    The speed of the drone has been increased, but it's still not the max
    speed the drone can reach. This function will be available soon.

--- drone_features/control.py
+++ drone_features/control.py
@@ -3,6 +3,7 @@ from olympe.messages.ardrone3.Sound import StartAlertSound, StopAlertSound
 from olympe.messages.ardrone3.PilotingState import FlyingStateChanged
 from olympe.messages.camera import set_alignment_offsets, reset_alignment_offsets
 from olympe.messages.gimbal import set_offsets, start_offsets_update, stop_offsets_update, calibrate, set_target
+from olympe.messages.common.Calibration import PitotCalibration, MagnetoCalibration
 import time
 
 class Command:
@@ -26,10 +27,10 @@ class Command:
         self.drone(PCMD(flag=1, roll=-30, pitch=0, yaw=0, gaz=0, timestampAndSeqNum=1))
 
     def forward(self):
-        self.drone(PCMD(flag=1, roll=0, pitch=30, yaw=0, gaz=0, timestampAndSeqNum=1))
+        self.drone(PCMD(flag=1, roll=0, pitch=70, yaw=0, gaz=0, timestampAndSeqNum=1))
         
     def backward(self):
-        self.drone(PCMD(flag=1, roll=0, pitch=-30, yaw=0, gaz=0, timestampAndSeqNum=1))
+        self.drone(PCMD(flag=1, roll=0, pitch=-70, yaw=0, gaz=0, timestampAndSeqNum=1))
 
     def rotate_left(self):
         self.drone(PCMD(flag=1, roll=0, pitch=0, yaw=-100, gaz=0, timestampAndSeqNum=1))
@@ -38,10 +39,10 @@ class Command:
         self.drone(PCMD(flag=1, roll=0, pitch=0, yaw=100, gaz=0, timestampAndSeqNum=1))
 
     def up(self):
-        self.drone(PCMD(flag=1, roll=0, pitch=0, yaw=0, gaz=10, timestampAndSeqNum=1))        
+        self.drone(PCMD(flag=1, roll=0, pitch=0, yaw=0, gaz=70, timestampAndSeqNum=1))        
 
     def down(self):
-        self.drone(PCMD(flag=1, roll=0, pitch=0, yaw=0, gaz=-10, timestampAndSeqNum=1))  
+        self.drone(PCMD(flag=1, roll=0, pitch=0, yaw=0, gaz=-70, timestampAndSeqNum=1))  
 
     def stop(self):
         self.drone(CancelMoveBy())
@@ -64,3 +65,6 @@ class Command:
 
     def camera_calibrate(self):
         self.drone(calibrate(gimbal_id=0))
+
+    def drone_calibrate(self):
+        self.drone(MagnetoCalibration(calibrate=1))
